0d4459f07c7c966c379388f190cbd9c112d22ecd
Tracey Jaquith <tracey@archive.org>
2025-12-01 19:45:55 -0800

    coverage workaround

--- test/example.test.js
+++ test/example.test.js
@@ -1,9 +1,11 @@
 // eslint-disable-next-line object-curly-newline
 import { describe, it, expect, run } from 'https://raw.githubusercontent.com/internetarchive/dyno/main/test/test.js'
 
+import hi from './lib.js'
 
 describe('test imports', () => {
   it('importing', () => {
+    hi()
     expect(typeof expect).toEqual('function')
     expect(typeof nope).toEqual('undefined')
   })
---
+++ test/lib.js
@@ -0,0 +1,4 @@
+export default function hi() {
+  // eslint-disable-next-line no-console
+  console.log('hi')
+}
