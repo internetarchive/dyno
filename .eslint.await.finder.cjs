d2013c634846479b207cbc8d72ab6418046fbd74
Tracey Jaquith <tracey@archive.org>
2026-03-27 17:26:34 -0700

    lint fixes

--- .eslint.await.finder.cjs
+++ .eslint.await.finder.cjs
@@ -1,15 +1,17 @@
 // this is <only> use to find missing `await` in front of calling `async` functions
+// eslint-disable-next-line no-undef
 module.exports = {
   root: true,
   env: {
     node: true,
   },
   globals: {
-    Promise: 'readonly'
+    Promise: 'readonly',
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
     sourceType: 'module',
+    // eslint-disable-next-line no-undef
     tsconfigRootDir: __dirname,
     project: ['./.eslint.await.finder.tsconfig.json'],
   },
