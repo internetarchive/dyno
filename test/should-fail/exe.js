a6fa1ce7838cc918eac4c4ca4957e755711da37d
Tracey Jaquith <tracey@archive.org>
2026-05-09 00:17:08 -0700

    comments, re-indents, etc (nothing substantitive)

--- test/should-fail/exe.js
+++ test/should-fail/exe.js
@@ -1,6 +1,6 @@
 // lint-test: deno-check-only
 // eslint-disable-next-line import/no-unresolved
-import { exe } from 'https://av.dev.archive.org/js/util/cmd.js'
+import { exe } from 'https://av.archive.org/js/util/cmd.js'
 
 exe('ls')
 exe('pwd')
